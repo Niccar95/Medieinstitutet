ce13b02156b1c2df2fa34130094db0868e6cae6a
Niccar95 <niccar.95@outlook.com>
2023-11-13 13:57:39 +0100

    changes

---
+++ Föreläsning7-övningar/src/js/main.js
@@ -0,0 +1,44 @@
+import './../scss/style.css'
+
+
+
+class Person {
+  firstName;
+  lastName;
+  age;
+
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+
+window.onload = function (){
+  const person = new Person("Nicolas", "Carrasco", "27");
+  console.log(person);
+  const personInfo = document.createElement("p");
+  document.body.appendChild(personInfo);
+
+  personInfo.innerHTML = person.age;
+
+  const person2 = new Person("Antonia", "Carrasco", "14");
+  console.log(person2);
+
+  const personList = [person, person2];
+
+  for (let i = 0; i < personList.length; i++) {
+
+    
+    const unorderedList = document.createElement("ul");
+    const list = document.createElement("li");
+    document.body.appendChild(unorderedList);
+    unorderedList.appendChild(list);
+
+    list.innerHTML = personList[i].firstName + " " + personList[i].lastName + " " + personList[i].age;
+  
+}
+
+}
+
